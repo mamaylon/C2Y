4282bd77b56ec1f739ee4662f5e20a1f96fa6d9e
Maylon <macedomaylon@gmail.com>
2018-07-04 15:35:24 -0300

    2 etapa

--- database/migrations/2018_07_04_152621_del_photo2.php
+++ database/migrations/2018_07_04_152621_del_photo2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DelPhoto extends Migration
+class DelPhoto2 extends Migration
 {
     /**
      * Run the migrations.
@@ -14,9 +14,15 @@ class DelPhoto extends Migration
     public function up()
     {
         Schema::table('courses', function (Blueprint $table) 
-        { 
+        {
             $table->dropColumn('photo');
         });
+
+        Schema::table('lessons', function (Blueprint $table) 
+        {
+            $table->text('photo')->nullable();            
+        });
+
     }
 
     /**
